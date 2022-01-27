a23b8f0c7e8bee5f820127c9324fdfc2f163a793
Mariia Dryha <m.pishchanska@gmail.com>
2022-01-28 00:39:25 +0300

    add task #2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const addListElem = document.querySelector("#ingredients");
+console.log(addListElem);
+
+const addCreateElem = ingredients.map(ingredient => {
+
+  const createElem = document.createElement("li");
+    
+  createElem.textContent = ingredient;
+  createElem.classList.add('item');
+  addListElem.append(createElem);
+  console.log(createElem);
+});
+
+
