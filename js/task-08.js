408f4775d7f8f16ffb0dabc23df1e48b8a340bcd
Mariia Dryha <m.pishchanska@gmail.com>
2022-02-03 22:56:20 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,33 @@
+const form = document.querySelector('form.login-form');
+form.addEventListener('submit', onFormSubmit);
+ 
+function onFormSubmit(event) {
+ event.preventDefault();
+
+    const formElem = event.currentTarget.elements;
+      
+    if (formElem.email.value === '' || formElem.password.value === '') {
+        alert('Все поля должны быть заполнены!');
+    };
+    
+    const formData = new FormData(event.currentTarget);  
+    let newObjectData = Object.fromEntries(formData);
+     console.log(newObjectData);
+
+   
+     form.reset();
+   
+    
+    // другой вариант? =====>
+    
+//  const formData = new FormData(event.currentTarget);
+//     formData.forEach((value, name) => {
+//         console.log(name, value);
+      
+//     })
+
+}
+
+
+
 
