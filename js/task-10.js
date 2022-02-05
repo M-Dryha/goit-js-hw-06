bf8d8593f14e2addcef58f01d8537949dc8b7813
Mariia Dryha <m.pishchanska@gmail.com>
2022-02-05 15:40:01 +0300

    add task #10 with error

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const inputElem = document.querySelector('input');
+
+const buttonCreate = document.querySelector('[data-create]');
+const buttonDestroy = document.querySelector('[data-destroy]');
+const boxesElem = document.querySelector('#boxes');
+
+
+inputElem.addEventListener('input', (changeInput));
+buttonCreate.addEventListener('click', (createBoxes));
+  
+function changeInput() {
+  
+console.log(inputElem.value);
+ }
+
+const amount = inputElem.value;
+ const basicSize = 30;
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    const size = basicSize + i * 10;
+    div.style.offsetWidth = size + 'px';
+  
+    div.style.backgroundColor = getRandomHexColor();
+    boxesElem.appendChild(div);
+    console.log(boxesElem);
+
+
+  }
+
+}
+  
+   
+
